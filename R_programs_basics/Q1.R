7622617ba3e1cc82d01c182ad12e02c834b2dcf1
Sudip Manna <sudip.manna2024@uem.edu.in>
2026-03-27 23:19:33 +0530

    Update Q1.R

--- R_programs_basics/Q1.R
+++ R_programs_basics/Q1.R
@@ -1,13 +1,7 @@
-
 df <- read.csv("data.csv")
-
 colSums(is.na(df))
-
 df_clean <- na.omit(df)
-
 df$Age[is.na(df$Age)] <- mean(df$Age, na.rm=TRUE)
-
 mode_value <- names(sort(table(df$Gender), decreasing=TRUE))[1]
 df$Gender[is.na(df$Gender)] <- mode_value
-
 df <- df[!duplicated(df), ]
